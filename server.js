4db3584c79fbffa395385e4eac405827fb43298c
aniketjain2001 <35770166+sakshijainn@users.noreply.github.com>
2022-07-24 00:39:23 +0530

    MongoDB connected

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//connect DataBase
+connectDB();
 
 //single end point to test it out ( basic express set-up-test)
 app.get("/", (req, res) => res.send("API running"));
