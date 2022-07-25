b04244ef6ba3cc568a6afde7ee93bb9d6356491d
aniketjain2001 <35770166+sakshijainn@users.noreply.github.com>
2022-07-26 01:31:27 +0530

    user registeration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
+const gravatar = require("gravatar");
+const bcrypt = require("bcrypt");
+
 //@route   POST api/users
 //@desc    Register user
 //@access  public
@@ -13,12 +17,45 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.status(400).json({ errors: errors.array() });
     }
-    res.send("Users route");
+    const { name, email, password } = req.body;
+    try {
+      //see if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      //get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({ name, email, avatar, password });
+
+      //encrypt password
+
+      //1. do hashing
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      //2. save in database
+      await user.save();
+
+      //return jsonwebtoken
+
+      res.send("User Registered");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
