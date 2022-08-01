619ddffb1e98e53aa0bb6305aaa97beef54eef93
aniketjain2001 <35770166+sakshijainn@users.noreply.github.com>
2022-08-01 18:48:51 +0530

    get profile by userID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -122,4 +122,26 @@ router.get("/", async (req, res) => {
   }
 });
 
+// @route GET api/profile/user/:user_id
+// @desc  get profile by userID
+// @access Public
+
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.find({ user: req.params.user_id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+    if (!profile)
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "objectId") {
+      return res.status(400).json({ msg: "Profile : Not Found" });
+    }
+    res.status(500).json("Server Error");
+  }
+});
+
 module.exports = router;
